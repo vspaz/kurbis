ad24829d7d126cf7187fd74ddcbc5f424583a842
vspaz <vladimir.edelman@gmail.com>
2023-01-19 00:27:15 +0100

    add 'app.Run'

--- app/app.go
+++ app/app.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"github.com/google/uuid"
+	"github.com/vspaz/simplelogger/pkg/logging"
+	"kurbis/job"
+	"kurbis/task"
+)
+
+func Run() {
+	logger := logging.GetJsonLogger("info").Logger
+	task_1 := task.Task{
+		Id:     uuid.New(),
+		Name:   "Task-1",
+		State:  job.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
+	logger.Info(task_1)
+}
