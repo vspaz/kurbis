c0014897ded3d42f119f4bd707ccacf949a3ceb5
vspaz <vladimir.edelman@gmail.com>
2023-01-18 20:07:51 +0100

    extend Task struct

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module kurbis
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/docker/go-connections v0.4.0
+	github.com/google/uuid v1.3.0
+)
+
+require github.com/stretchr/testify v1.8.1 // indirect
--- task/task.go
+++ task/task.go
@@ -1,12 +1,19 @@
 package task
 
 import (
+	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 	"kurbis/job"
 )
 
 type Task struct {
-	Id    uuid.UUID
-	Name  string
-	State job.State
+	Id            uuid.UUID
+	Name          string
+	State         job.State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy string
 }
