0f72d44fabc981089ea1c208aca7eb00ed67032f
vspaz <vladimir.edelman@gmail.com>
2023-01-19 00:17:42 +0100

    adding Node object

--- node/node.go
+++ node/node.go
@@ -1,1 +1,11 @@
 package node
+
+type Node struct {
+	Name            string
+	Ip              string
+	Memory          int
+	MemoryAllocated int
+	Disk            int
+	DiskAllocated   int
+	TaskCount       int
+}
