a07626bc23f9c2d2a2d706f82c7ac3e96d5cc329
vspaz <vladimir.edelman@gmail.com>
2023-01-19 00:00:30 +0100

    add Worker object

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,13 @@
 package worker
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"kurbis/task"
+)
+
+type Worker struct {
+	Queue      queue.Queue
+	uuidToTask map[uuid.UUID]task.Task
+	taskCount  int
+}
