dd6f63efc175de2cae3cb841a4df9a4a9d7df62a
vspaz <vladimir.edelman@gmail.com>
2023-01-18 20:10:57 +0100

    add task.Event

--- task/event.go
+++ task/event.go
@@ -1,1 +1,14 @@
 package task
+
+import (
+	"github.com/google/uuid"
+	"kurbis/job"
+	"time"
+)
+
+type Event struct {
+	Id        uuid.UUID
+	State     job.State
+	Timestamp time.Time
+	Task      Task
+}
