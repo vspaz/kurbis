302af80e0bc1fa84bb6a5154aa0a272c178ada05
vspaz <vladimir.edelman@gmail.com>
2023-01-19 00:12:02 +0100

    add Manager object

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,16 @@
 package manager
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"kurbis/task"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	NameToTasks   map[string][]task.Task
+	NameToEvents  map[string][]task.Task
+	Workers       []string
+	WorkerToTasks map[string][]uuid.UUID
+	TaskToWorker  map[uuid.UUID]string
+}
