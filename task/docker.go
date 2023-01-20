a15989e0904f4b19d526237689983da74ede3b56
vspaz <vladimir.edelman@gmail.com>
2023-01-20 19:28:04 +0100

    add cocker.Run function

--- task/docker.go
+++ task/docker.go
@@ -1,11 +1,19 @@
 package task
 
-import "github.com/docker/docker/client"
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
+)
 
 type Docker struct {
 	Client      *client.Client
 	Config      Config
 	ContainerId string
+	Logger      *logrus.Logger
 }
 
 type Result struct {
@@ -13,4 +21,19 @@ type Result struct {
 	Action      string
 	ContainerId string
 	Result      string
+}
+
+func (d *Docker) Run() Result {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(
+		ctx,
+		d.Config.Image,
+		types.ImagePullOptions{},
+	)
+	if err != nil {
+		d.Logger.Errorf("Error pulling image %s %s", d.Config.Image, err)
+		return Result{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
+	return Result{Error: nil}
 }
