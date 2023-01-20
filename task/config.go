3f67196de89014257e64bd129f182e3de0f36abb
vspaz <vladimir.edelman@gmail.com>
2023-01-20 19:12:46 +0100

    add task.Config

--- task/config.go
+++ task/config.go
@@ -1,1 +1,14 @@
 package task
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	Cmd           []string
+	Image         string
+	Memory        int64
+	Disk          []string
+	Env           []string
+	RestartPolicy string
+}
